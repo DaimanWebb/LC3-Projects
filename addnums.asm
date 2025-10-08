9857f5f4d9dfc82f1e8576cf5b05ef064758811e
DaimanWebb <webbdaiman@gmail.com>
2025-10-08 16:45:33 -0700

    addnums.asm has the hypothetical code at the bottom now

--- addnums.asm
+++ addnums.asm
@@ -2,22 +2,21 @@
         AND R1, R1, x0
         AND R4, R4, x0
         ADD R4, R4, #10
-        LEA R2, PTR
+        LEA R2, x0FC          ; hardcoded address of first number ; could use a label here
 LOOP    LDR R3, R2, x0
         ADD R2, R2, #1
         ADD R1, R1, R3
         ADD R4, R4, #-1
         BRp LOOP
         HALT
-
-;        AND R6, R6, #0      ; PURELY HYPOTHETICAL IF WE TRIED TO LOAD --- WITH A VALUE FROM R6, LET'S SAY
-;        ADD R6, R6, #5      ; CONT.
-;        ST R6, ---
-;---                        ;   Why does it say the following?  :
-                            ;       Multiple label at the same memory location. 
-                            ;       LC3 addnums.asm(12,1): Label --- and PTR are at the same memory 
-                            ;       Illegal label name. LC3 addnums.asm(12,1): Label name is illegal
                             
 PTR     .FILL   x0FC       
         .END           
 
+;         AND R6, R6, #0      ; PURELY HYPOTHETICAL IF WE TRIED TO LOAD --- WITH A VALUE FROM R6, LET'S SAY
+;         ADD R6, R6, #5      ; CONT.
+;         ST R6, ---
+; ---     .FILL   #36         ;   Why does it say the following?  :
+                              ;       Multiple labels at the same memory location. 
+                              ;       LC3 addnums.asm(12,1): Label --- and PTR are at the same memory 
+                              ;       Illegal label name. LC3 addnums.asm(12,1): Label name is illegal
